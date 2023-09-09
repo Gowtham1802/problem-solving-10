55c2dd6061b826d2e9fa5174d077eff1849104c2
Gowtham1802 <gowthama7575@gmail.com>
2023-09-09 15:38:26 +0530

    add

--- script.js
+++ script.js
@@ -89,3 +89,123 @@ function findLargestEvenNumber(arr) {
 // Expected output:The average is 3.5.
 
 
+function calculateAverage() {
+  var inputNumbers = document.getElementById("inputNumbers").value;
+  var numbersArray = inputNumbers.split(",").map(Number);
+  
+  if (numbersArray.some(isNaN)) {
+      document.getElementById("result").innerHTML = "Please enter valid numbers.";
+  } else {
+      var sum = 0;
+      for (var i = 0; i < numbersArray.length; i++) {
+          sum += numbersArray[i];
+      }
+      var average = sum / numbersArray.length;
+      document.getElementById("result").innerHTML = "The average is " + average.toFixed(2);
+  }
+}
+
+
+
+// 15.You are given a list of items. Write a program that randomly selects an item from the
+// list and displays it on a web page.
+// Given input:Apple, Banana, Orange, Grape, Mango
+// Expected output:When you click submit button,it randomly selects an item from the list.
+
+
+function selectRandomItem() {
+  var items = ["Apple", "Banana", "Orange", "Grape", "Mango"];
+  var randomIndex = Math.floor(Math.random() * items.length);
+  var selectedItem = items[randomIndex];
+  document.getElementById("result").innerHTML = "Randomly selected item: " + selectedItem;
+}
+
+
+// 16.You are given a string. Write a program that counts the number of vowels in the string
+// and displays the result on a web page.
+// Given input:Skill safari
+// Expected Output:Number of vowels: 4
+
+
+function countVowels() {
+  var inputString = document.getElementById("inputString").value;
+  var vowelCount 
+  // Convert the input string to lowercase to make the comparison case-insensitive
+  inputString = inputString.toLowerCas
+    for (var i = 1; i < inputString.length; i++) {
+        var currentChar = inputString.charAt(i);
+        if (currentChar === 'a' || currentChar === 'e' || currentChar === 'i' || currentChar === 'o' || currentChar === 'u') {
+            vowelCount++;
+        }
+    }
+
+ document.getElementById("result").innerHTML = "Number of vowels: " + vowelCount;
+}
+
+
+
+// 17.You are given an array of numbers. Write a program that finds the median of the
+// numbers and displays the result on a web page.
+// Given input:2,5,7,1,45,8,7
+// Expected Output:Median: 7
+
+
+
+
+// 18.You are given two strings. Write a program that checks if the two strings are anagrams
+// (i.e., the two strings have the same characters but in a different order) and displays the
+// result on a web page.
+// Given input:Skill safari
+// Expected output:The strings are not anagrams.
+
+
+
+
+// 19.You are given an array of numbers. Write a program that finds the largest and smallest
+// odd numbers in the array and displays the results on a web page.
+// Given input:2,4,5,7,2,3
+// Expected output:Largest odd number: 7, Smallest odd number: 3
+
+
+
+function findOddNumbers() {
+  var inputNumbers = document.getElementById("inputNumbers").value;
+  var numbersArray = inputNumbers.split(",").map(Number);
+
+  if (numbersArray.some(isNaN)) {
+      document.getElementById("result").innerHTML = "Please enter valid numbers.";
+  } else {
+    var oddNumbers = numbersArray.filter(function(num) {
+      return num % 2 !== 0;
+  });
+
+  if (oddNumbers.length === 0) {
+    document.getElementById("result").innerHTML = "No odd numbers found in the array.";
+  } else {
+      var largestOdd = Math.max(...oddNumbers);
+      var smallestOdd = Math.min(...oddNumbers);
+      document.getElementById("result").innerHTML = "Largest odd number: " + largestOdd + ", Smallest odd number: " + smallestOdd;
+    }
+  }
+}
+
+
+
+// 20.You are given a list of items. Write a program that removes all duplicates from the list
+// and displays the result on a web page.
+// Given input:apple, banana, cherry, apple, date, banana, elderberry
+// Expexted Output:apple,banana,cherry,,date,elderberry
+
+
+
+function removeDuplicates() {
+  var inputItems = document.getElementById("inputItems").value;
+  var itemsArray = inputItems.split(",");
+  // Use a Set to remove duplicates and then convert it back to an array
+  var uniqueItems = [...new Set(itemsArray)];
+
+  // Join the unique items back into a string
+  var resultString = uniqueItems.join(", ");
+            
+  document.getElementById("result").innerHTML = resultString;
+}
